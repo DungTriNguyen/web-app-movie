56584e679a27be0edf35126c34f03fffb1cd18b1
FE DEV Duc Manh <manh.duc@hubcom.tech>
2025-03-24 18:30:01 +0700

    fix: image

--- src/components/advance.tsx
+++ src/components/advance.tsx
@@ -7,7 +7,7 @@ export default function Advance() {
             className="flex items-center relative px-4 md:px-20 w-full h-[536px] "
         >
             <div className="flex w-full h-full justify-center gap-12 self-stretch flex-col-reverse md:flex-row relative">
-                <div className="flex flex-1 flex-col justify-center items-end gap-12 bg-[url('/images/token-trade.png')] bg-contain bg- bg-no-repeat">
+                <div className="flex flex-1 flex-col justify-center items-end gap-12 overflow-hidden">
                     {/* <Image
                         src="/images/token-trade.png"
                         alt="token trade"
@@ -15,8 +15,9 @@ export default function Advance() {
                         width={1390}
                         height={868}
                     /> */}
-                    <div className="bg-[url('/images/token-trade.png')] bg-cover bg-center bg-no-repeat"></div>
-                    <div className="bg-gradient-to-b from-[#1C2230]/0 to-[#1C2230] w-[1390px] h-[536px] absolute top-0"></div>
+                    <div className="bg-[url('/images/token-trade.png')] bg-cover bg-right-top
+ bg-no-repeat flex-1 flex w-[1390px] h-[536px] absolute top-0 rounded-t-2xl shadow-[-20px_-20px_0px_0px_rgba(17,24,39,0.8)] border-2 border-border aspect-[479/268]"></div>
+                    <div className="bg-gradient-to-b  from-[#1C2230]/0 to-[#1C2230] w-[1390px] h-[536px] absolute top-0"></div>
                 </div>
                 <div className="flex-1 justify-center flex flex-col py-9 gap-6">
                     <h1 className="max-w-[600px] text-4xl md:text-5xl font-bold flex flex-col gap-4 items-start self-stretch">
