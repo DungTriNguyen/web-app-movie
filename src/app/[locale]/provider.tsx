74bf2a9c5ac89d361261cdb2551b71717ea159b7
FE DEV Duc Manh <manh.duc@hubcom.tech>
2025-03-26 17:18:08 +0700

    fix: remove uneccesary file

--- src/app/[locale]/provider.tsx
+++ src/app/[locale]/provider.tsx
@@ -5,7 +5,6 @@ export default function Providers({ children }: { children: React.ReactNode }) {
     const queryClient = new QueryClient()
     return (
         <>
-
             <QueryClientProvider client={queryClient}>
                 {children}
             </QueryClientProvider>
