308850a435b09ab38be1bf678df59c0cb2cb9885
khoidoan <nguyenkhoi1610@gmail.com>
2025-08-28 14:47:48 +0700

    update cdn

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'assets.dexspace.io',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
